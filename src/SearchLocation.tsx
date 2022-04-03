865fbb4213c6a4218906c28f746e799834226de4
tkyk.niimura <tkyk.niimura@gmai.com>
2022-04-03 09:27:06 +0900

    fix search location

--- src/SearchLocation.tsx
+++ src/SearchLocation.tsx
@@ -20,31 +20,8 @@ type propType = {
 const SearchLocation: VFC<propType> = ({ setLocation }) => {
   const iconSize = '30px';
   const regexp = /^\s*(\d+(?:\.\d+)?)\s*,\s*(\d+(?:\.\d+)?)\s*$/;
-  const [enter, setEnter] = useState(false);
   const [address, setAddress] = useState('');
   const map = useMap();
-
-  const onMouseEnter = (e: MEvent) => {
-    console.log(e.currentTarget.getBoundingClientRect());
-
-    setEnter(true);
-  };
-
-  const onMouseLeave = (e: MEvent) => {
-    console.log(e.currentTarget.getBoundingClientRect());
-    const rect = e.currentTarget.getBoundingClientRect();
-    if (
-      rect.left <= e.clientX &&
-      e.clientX <= rect.left + rect.width &&
-      rect.top <= e.clientY &&
-      e.clientY <= rect.top + rect.height
-    ) {
-      return;
-    }
-
-    setEnter(false);
-  };
-
   const onClick = () => {
     const result = regexp.exec(address);
     if (result) {
@@ -52,44 +29,32 @@ const SearchLocation: VFC<propType> = ({ setLocation }) => {
       const latLng = { lat: parseFloat(lat), lng: parseFloat(lng) };
       map.flyTo(latLng, 14);
       setLocation(latLng);
-      setAddress('');
     }
 
     normalize(address).then((result) => {
-      console.log(address);
       const { lat, lng } = result;
       if (lat && lng) {
         map.flyTo({ lat, lng }, 14);
         setLocation({ lat, lng });
-        setAddress('');
       }
     });
   };
 
   return (
-    <Control
-      position="topleft"
-      style={{ backgroundColor: '#FFF', height: iconSize }}
-    >
-      <div
-        onMouseEnter={(e) => onMouseEnter(e)}
-        onMouseLeave={(e) => onMouseLeave(e)}
-      >
-        {!enter ? (
-          <FaSearchLocation size={iconSize} />
-        ) : (
-          <div className="search-location">
-            <input
-              type="text"
-              value={address}
-              size={40}
-              onChange={(e) => setAddress(e.target.value)}
-              onKeyDown={(e) => e.key === 'Enter' && onClick()}
-            />
+    <Control position="topleft" style={{ backgroundColor: '#FFF' }}>
+      <div className="search-location" style={{ height: iconSize }}>
+        <FaSearchLocation className="search-location-icon" size={iconSize} />
+        <div className="search-location-input">
+          <input
+            type="text"
+            value={address}
+            size={40}
+            onChange={(e) => setAddress(e.target.value)}
+            onKeyDown={(e) => e.key === 'Enter' && onClick()}
+          />
 
-            <button onClick={() => onClick()}>検索</button>
-          </div>
-        )}
+          <button onClick={() => onClick()}>検索</button>
+        </div>
       </div>
     </Control>
   );
